e17c3e8aabcfa4b7b67dcbd15b8df3b4dc6188ff
stelmakov_i <stelmakovilya.team@anvanto.com>
2018-10-09 00:10:03 +0300

    2

--- wp-content/themes/sogin/index.php
+++ wp-content/themes/sogin/index.php
@@ -2,40 +2,75 @@
 <img src="<?php echo get_field( 'banner_1');?>" alt="shkafi-kupe">
 <div class="empty_separator"></div>
 <div class="main_categories">
-    <div class="row">
+    <div class="category_row">
         <div class="category">
             <a class="no-hover no-border" href="/kitchens/">
+                <div class="hover"></div>
                 <img src="/wp-content/themes/sogin/img/home_kitchen.jpg">
             </a>
         </div>
         <div class="category">
             <a class="no-hover no-border" href="/garderobi/">
+                <div class="hover"></div>
                 <img src="/wp-content/themes/sogin/img/home_garderob.jpg">
             </a>
         </div>
         <div class="category">
             <a class="no-hover no-border" href="/gorki/">
+                <div class="hover"></div>
                 <img src="/wp-content/themes/sogin/img/home_gorki.jpg">
             </a>
         </div>
     </div>
     <div class="empty_separator"></div>
-    <div class="row">
+    <div class="category_row">
         <div class="category">
             <a class="no-hover no-border" href="#">
+                <div class="hover"></div>
                 <img src="/wp-content/themes/sogin/img/home_mebel.jpg">
             </a>
         </div>
         <div class="category">
             <a class="no-hover no-border" href="#">
+                <div class="hover"></div>
                 <img src="/wp-content/themes/sogin/img/home_raspil.jpg">
             </a>
         </div>
         <div class="category">
             <a class="no-hover no-border" href="#">
+                <div class="hover"></div>
                 <img src="/wp-content/themes/sogin/img/home_potolki.jpg">
             </a>
         </div>
     </div>
 </div>
+<div class="empty_separator_mini"></div>
+<div class="home_arrow_right arrow_big"><div class="arrow_inner"></div><img src="/wp-content/themes/sogin/img/arrow_right.svg"></div>
+<div class="home_benefits">
+    <div class="item">
+        <p class="top">Точные сроки</p>
+        <p class="bottom">от 1 дня</p>
+    </div>
+    <div class="item">
+        <p class="top">Собственное производство</p>
+        <p class="bottom">даем лучшую цену</p>
+    </div>
+    <div class="item">
+        <p class="top">Доставка и сборка</p>
+        <p class="bottom">бесплатно</p>
+    </div>
+    <div class="item">
+        <p class="top">Рассрочка 0%</p>
+        <p class="bottom">без участия банка и переплат</p>
+    </div>
+    <div class="item">
+        <p class="top">Дизайнер- замерщик</p>
+        <p class="bottom">бесплатный выезд</p>
+    </div>
+    <div class="item">
+        <p class="top">Подарок</p>
+        <p class="bottom">каждому покупателю</p>
+    </div>
+</div>
+<div class="home_arrow_right arrow_small"><div class="arrow_inner"></div><img src="/wp-content/themes/sogin/img/arrow_right.svg"><p>С О Г И Н - Л Ю К С</p></div>
 <?php get_footer(); ?>
